f37c288de761d7eaa16434f9e0eb8d28f277b1da
gnanasurya <gnanasurya2@gmail.com>
2020-01-15 22:44:03 +0530

    added changes

--- level_2.py
+++ level_2.py
@@ -1,8 +1,8 @@
 string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 length = len(string)
 new_string = ''
-#Since from the picture it's clear that every character is being shifted two characters in the front.
-for i in range(0,length,1):
+# Since from the picture it's clear that every character is being shifted two characters in the front.
+for i in range(0, length, 1):
     num = ord(string[i])
     if (num >= 97 and num <= 122):
         num = num + 2
@@ -11,11 +11,12 @@ for i in range(0,length,1):
     char = chr(num)
     new_string = new_string + char
 print(new_string)
-#the output of new_string suggests us to apply the same to the url.
-new_string =''
-url_string ="map"
+# the output of new_string suggests us to apply the same to the url.
+new_string = ''
+url_string = "map"
+print("hrllo")
 length = len(url_string)
-for i in range(0,length,1):
+for i in range(0, length, 1):
     num = ord(url_string[i])
     if (num >= 97 and num <= 122):
         num = num + 2
